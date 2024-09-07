2dc6e569e3533b7692d2b6564e874735ec117531
Dani <dustyglass@gmail.com>
2024-09-06 20:10:19 -0400

    site finished

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-async function ask() {
-    const container = document.querySelector("#reply")
-    const url = "https://taylorswiftapi.onrender.com/get"
+async function askTaylor(event) {
+    event.preventDefault()
+    const formArea = document.getElementById("ask")
+
+    if(formArea.value.trim() === ''){
+        alert('Please ask Taylor a question!');
+    }else{
+    const container = document.querySelector("#replyContainer")
+   const url = "https://taylorswiftapi.onrender.com/get"
+//    const url = "https://pokeapi.co/api/v2/pokemon/ditto"
     
     try{
         const response = await axios.get(url)
-        container.innerHTML = response.quote
+        container.innerHTML = `<div class="replyArea">
+                <h3 class="mother">Mother says:</h3>
+                <p id="reply">${response.data.quote}</p>
+            </div>`
+    
     }catch(error){
         container.innerHTML = `Sorry, the old Taylor can't come to the phone right now. Why? Because she's dead! And also ${error.message}`
         console.log(error)
     }
-
+    }
 }
